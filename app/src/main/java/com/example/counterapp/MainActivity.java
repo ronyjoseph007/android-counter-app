b93a438a51c3e43022ffd178f98b32fb7d2b6c63
rony joseph <ronyjoseph1998@gmail.com>
2020-11-10 06:43:56 -0800

    finished'

--- app/src/main/java/com/example/counterapp/MainActivity.java
+++ app/src/main/java/com/example/counterapp/MainActivity.java
@@ -3,12 +3,61 @@ package com.example.counterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public  class MainActivity extends AppCompatActivity {
+    int my_count=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Button btminus,btplus;
+        final TextView text,txreset;
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btminus=findViewById(R.id.btminus);
+        btplus=findViewById(R.id.btplus);
+        text=findViewById(R.id.text);
+        txreset=findViewById(R.id.txreset);
+
+        btminus.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                my_count=my_count-1;
+                text.setText(my_count+ "");
+            }
+
+
+
+        });
+
+        btplus.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+              my_count=my_count+1;
+                text.setText(my_count+ "");
+            }
+
+
+
+        });
+
+        txreset.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+              my_count=0;
+                text.setText(my_count+ "");
+            }
+
+
+
+        });
+
+
+
     }
 }
